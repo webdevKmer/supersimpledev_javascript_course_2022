0dccfa58f14646b853b98417bc5334b2d743c8b4
WebDev Kmer <webdev.kmer@gmail.com>
2023-09-02 16:40:30 +0100

    add lesson 1 and complete

---
+++ lesson1/1H.js
@@ -0,0 +1,18 @@
+// ----------- 1H ---------------
+const tshirt = 10
+const socks = 8
+const plates = 20
+
+let order = tshirt + plates + socks
+console.log("Order Total :", order);
+
+// ----------- 1I ---------------
+const account = 100
+const lunch = 20
+const dinner = 50
+const earnings = 200
+
+let saldo = account + earnings - lunch - dinner
+console.log("Your new Saldo is :", saldo);
+
+// ----------- 1J ---------------
